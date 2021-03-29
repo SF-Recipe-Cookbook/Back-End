333af6bd9bcf704101d16b2581809a361d848d49
Chad Parker <63312621+ChimpBite@users.noreply.github.com>
2021-03-29 13:15:18 -0600

    Added dependencies, Built Migrations

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -1,13 +1,31 @@
 exports.up = async (knex) => {
   await knex.schema.createTable('users', (users) => {
-    users.increments();
+    users.increments('id');
     users.string('username', 200).notNullable();
     users.string('password', 200).notNullable();
     users.string('email', 320).notNullable();
     users.timestamps(false, true);
   });
+  await knex.schema.createTable('recipes', (recipes) => {
+    recipes.increments('id');
+    recipes.string('name', 200).notNullable();
+    recipes.string('category', 200).notNullable();
+    recipes.text('description').notNullable();
+    recipes.text('ingredients').notNullable();
+    recipes.text('instructions').notNullable();
+    recipes.integer('prep_time').notNullable();
+    recipes.integer('cook_time').notNullable();
+    recipes.timestamps(false, true);
+    recipes
+      .integer('user_id')
+      .references('id')
+      .inTable('users')
+      .onDelete('cascade')
+      .onUpdate('cascade');
+  });
 };
 
 exports.down = async (knex) => {
+  await knex.schema.dropTableIfExists('recipes');
   await knex.schema.dropTableIfExists('users');
 };
